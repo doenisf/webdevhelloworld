dfbbbbb5254f2757c069bc44c9ad5db186398d1c
Simon Fröhner <simon@wk-froehner.de>
2020-09-11 16:25:57 +0200

    Added script.js for redirect on button cklick

---
+++ script.js
@@ -0,0 +1,7 @@
+function gitRedirect() {
+    location.href ="https://github.com/doenisf";
+}
+
+function instaRedirect() {
+    location.href = "https://www.instagram.com/simon.froehner/?hl=de";
+}
