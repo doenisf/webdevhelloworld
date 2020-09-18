264ac383ea44b54c2f9a2ddc5aa85a4fbeb61871
Simon Fröhner <simon@wk-froehner.de>
2020-09-18 16:42:55 +0200

    Some easy starter scripts added

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ function gitRedirect() {
 function instaRedirect() {
     location.href = "https://www.instagram.com/simon.froehner/?hl=de";
 }
+
+const clickMeButton = document.querySelector('#click-me');
+
+clickMeButton.addEventListener("click", () => {
+    alert("Button Clicked!!!!");
+});
+
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+
+    const values = Object.fromEntriers(new FormData(e.target));
+    console.log(values);
+})
